908c5021d83c33d8d493390ff14df74354cc60c7
dapoadedire <adedireadedapo19@gmail.com>
2023-01-12 09:54:36 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,133 @@
+import { useState } from "react";
+import "normalize.css";
+import "./App.css";
+import ctl from "@netlify/classnames-template-literals";
+import Jokes from "./Jokes";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import arrow up and arrow down
+
+
+import {faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+
+function App() {
+  const [activeIndex, setActiveIndex] = useState([]);
+
+  const handleShow = (index) => {
+    if (activeIndex.includes(index)) {
+      setActiveIndex(activeIndex.filter((i) => i !== index));
+    } else {
+      setActiveIndex([...activeIndex, index]);
+    }
+  };
+
+  return (
+    <div className={sectionStyles}>
+      <div className={accordionStyles}>
+        <h2 className={titleStyles}>Jokes World :)</h2>
+        {Jokes.map((data, index) => (
+          <Item
+            key={data.id}
+            question={data.question}
+            active={activeIndex.includes(index)}
+            onShow={() => handleShow(index)}
+          >
+            {data.answer}
+          </Item>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Item({ question, children, active, onShow }) {
+  return (
+    <div className={containerChildStyles}>
+      <div className={itemStyles}>
+        <p className={questionStyles}>{question}</p>
+        <button
+          onClick={onShow}
+          className={`${buttonStyles} ${
+            active ? "bg-red-500" : "bg-green-500"
+          }`}
+        >
+          <FontAwesomeIcon icon={active ? faAngleUp : faAngleDown} />
+        </button>
+        
+      </div>
+
+      <div>
+        <p className={answerStyles}>{active && children}</p>
+      </div>
+    </div>
+  );
+}
+
+const sectionStyles = ctl(`
+  bg-gray-100
+  p-4
+  rounded
+  border-2
+
+  `);
+
+const itemStyles = ctl(`
+  flex
+  justify-between
+  items-center
+  `);
+
+const containerChildStyles = ctl(`
+  bg-gray-100
+  p-4
+  rounded
+  border-2
+  mb-4
+  `);
+
+const accordionStyles = ctl(`
+  bg-gray-100
+  w-8/10
+  md:w-2/3
+  lg:w-2/3
+  xl:w-3/5
+  // mobile screen
+
+
+  p-4
+  rounded
+  border-2
+  mx-auto
+  `);
+
+const titleStyles = ctl(`
+  text-3xl
+  text-center
+  mb-5
+  text-green-700
+  font-bold
+  
+  `);
+
+const buttonStyles = ctl(`
+  text-white
+  px-4
+  py-2
+  rounded
+  text-2xl
+  ml-1
+  my-0
+  
+  `);
+
+const questionStyles = ctl(`
+  text-base
+  text-gray
+  `);
+
+const answerStyles = ctl(`
+py-5
+  text-base
+  text-gray-700
+  `);
+
+export default App;
---
+++ src/Jokes.jsx
@@ -0,0 +1,163 @@
+const Jokes = [
+  {
+    id: 1,
+    question: "Why do programmers prefer dark mode?",
+    answer: "Because light attracts bugs.",
+  },
+  {
+    id: 2,
+    question: "Why do programmers always mix up Halloween and Christmas?",
+    answer: "Because Oct 31 equals Dec 25.",
+  },
+  {
+    id: 3,
+    question: "How do you know if a programmer is an extrovert?",
+    answer: "They constantly talk about their functions.",
+  },
+  {
+    id: 4,
+    question: "What do you call a snake that works in a computer factory?",
+    answer: "A python.",
+  },
+  {
+    id: 5,
+    question: "Why was the computer cold?",
+    answer: "It left its Windows open.",
+  },
+  {
+    id: 6,
+    question:
+      "Why did the programmer quit his job at the orange juice factory?",
+    answer: "He couldn't handle the constant pressure of the pulp.",
+  },
+  {
+    id: 7,
+    question: "What do you call a boomerang that doesn't come back?",
+    answer: "A stick.",
+  },
+  {
+    id: 8,
+    question: "What do you call a programmer who's always late to meetings?",
+    answer: 'A "Coder-do"',
+  },
+  {
+    id: 9,
+    question: "Why did the programmer quit his job at the juice factory?",
+    answer: "He couldn't handle the pressure of the pulp.",
+  },
+  {
+    id: 10,
+    question: "What do you call a programmer from Finland?",
+    answer: "A Finn-ish coder.",
+  },
+  {
+    id: 11,
+    question: "Why do programmers always mix up Halloween and Christmas?",
+    answer: "Because Oct 31 equals Dec 25.",
+  },
+  {
+    id: 12,
+    question: "Why do programmers prefer dark mode?",
+    answer: "Because light attracts bugs.",
+  },
+  {
+    id: 13,
+    question: "How do you know if a programmer is an extrovert?",
+    answer: "They constantly talk about their functions.",
+  },
+  {
+    id: 14,
+    question: "What do you call a snake that works in a computer factory?",
+    answer: "A python.",
+  },
+  {
+    id: 15,
+    question: "Why was the computer cold?",
+    answer: "It left its Windows open.",
+  },
+  {
+    id: 16,
+    question:
+      "Why did the programmer quit his job at the orange juice factory?",
+    answer: "He couldn't handle the constant pressure of the pulp.",
+  },
+  {
+    id: 17,
+    question: "Why do programmers always mix up Halloween and Christmas?",
+    answer: "Because Oct 31 equals Dec 25.",
+  },
+  {
+    id: 18,
+    question: "How do you know if a programmer is an extrovert?",
+    answer: "They constantly talk about their functions.",
+  },
+
+  {
+    id: 19,
+    question: "Why did the tomato turn red?",
+    answer: "Because it saw the salad dressing!",
+  },
+  {
+    id: 20,
+    question: "Why don't scientists trust atoms?",
+    answer: "Because they make up everything.",
+  },
+  {
+    id: 21,
+    question: "Why was the math book sad?",
+    answer: "Because it had too many problems.",
+  },
+  {
+    id: 22,
+    question: "Why did the banana go to the doctor?",
+    answer: "Because it wasn't peeling well.",
+  },
+  {
+    id: 23,
+    question: "Why did the Python programmer quit their job?",
+    answer: "They didn't get list-ed.",
+  },
+  {
+    id: 24,
+    question: "Why was the JavaScript developer sad?",
+    answer: 'Because they didn\'t know how to "null" handle their emotions.',
+  },
+  {
+    id: 25,
+    question: "Why did the programmer quit their job as a plumber?",
+    answer: "They didn't like debugging pipes.",
+  },
+  {
+    id: 26,
+    question: "Why was the programmer always calm during a crisis?",
+    answer: 'They always had a "plan B" ready.',
+  },
+  {
+    id: 27,
+    question: "What do you call a snake that works in a computer factory?",
+    answer: "A python.",
+  },
+  {
+    id: 28,
+    question: "Why was the computer cold?",
+    answer: "It left its Windows open.",
+  },
+  {
+    id: 29,
+    question:
+      "Why did the programmer quit his job at the orange juice factory?",
+    answer: "He couldn't handle the constant pressure of the pulp.",
+  },
+  {
+    id: 30,
+    question: "Why do programmers always mix up Halloween and Christmas?",
+    answer: "Because Oct 31 equals Dec 25.",
+  },
+  {
+    id: 31,
+    question: "How do you know if a programmer is an extrovert?",
+    answer: "They constantly talk about their functions.",
+  },
+];
+
+export default Jokes;
